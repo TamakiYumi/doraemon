01f9ba6be779faedf824bc4465b1daa29e68d95f
EC2 Default User <ec2-user@ip-172-31-22-107.us-east-2.compute.internal>
2021-01-21 02:37:01 +0000

    first commit

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class NewsController extends Controller
+{
+    public function add()
+    {
+        return view('admin.news.create');
+    }
+
+    public function create()
+    {
+        return redirect('admin/news/create');
+    }
+
+    public function edit()
+    {
+        return view('admin.news.edit');
+    }
+
+    public function update()
+    {
+        return redirect('admin/news/edit');
+    }
+}
